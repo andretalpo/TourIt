d3aca808b6ab6cc0077af2eae12d81f97e416d0a
andretalpo <andre.talpo@gmail.com>
2017-08-28 18:02:00 -0300

    Arrumado erros de id das telas

--- app/src/main/java/br/com/marcus/fernanda/andre/tourit/UsuarioAdmActivity.java
+++ app/src/main/java/br/com/marcus/fernanda/andre/tourit/UsuarioAdmActivity.java
@@ -35,15 +35,6 @@ public class UsuarioAdmActivity extends AppCompatActivity {
 
         database = FirebaseDatabase.getInstance().getReference();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         recyclerView = (RecyclerView) findViewById(R.id.usuariosAdmRecyclerView);
 
         listaUsuarios = new ArrayList<>();
