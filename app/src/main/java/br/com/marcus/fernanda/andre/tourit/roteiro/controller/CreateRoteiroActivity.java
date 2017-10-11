966f1957ce52fcebd7f6b98529ce87b6d220ee76
andretalpo <andre.talpo@gmail.com>
2017-10-11 17:35:56 -0300

    Mais e mais melhorias de layout

--- app/src/main/java/br/com/marcus/fernanda/andre/tourit/roteiro/controller/CreateRoteiroActivity.java
+++ app/src/main/java/br/com/marcus/fernanda/andre/tourit/roteiro/controller/CreateRoteiroActivity.java
@@ -13,6 +13,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
